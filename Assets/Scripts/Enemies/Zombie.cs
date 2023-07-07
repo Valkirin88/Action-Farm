ae1bc4c5ffb92f2a9b52e187e25ea8e5f4f30ed4
A_Sukhoverkhov <valkirin@rambler.ru>
2023-07-07 23:18:53 +0300

    AI works

--- Assets/Scripts/Enemies/Zombie.cs
+++ Assets/Scripts/Enemies/Zombie.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.AI;
+using System.Collections;
 
 public class Zombie : MonoBehaviour
 {
@@ -10,36 +12,79 @@ public class Zombie : MonoBehaviour
     [SerializeField]
     private Animator _animator;
 
+    [SerializeField]
+    private NavMeshAgent _agent;
+
+    [SerializeField]
+    private Transform[] _patrolPoints;
+
     private GameObject _player;
-    private float _speed =1f;
+    private float _runSpeed =1f;
+    private float _walkSpeed = 1f;
+    private int _quantityPoints;
+    private int _nextPoint;
+    private bool _isWalk;
+
+
+    private void Start()
+    {
+        _quantityPoints = _patrolPoints.Length;
+        Patrol();
+    }
 
     private void OnTriggerStay(Collider other)
         {
         if (other.GetComponent<PlayerView>())
         {
             _player = other.gameObject;
-            CatchPlayer();
+            FollowPlayer();
         }
-    }
-
-    private void CatchPlayer()
-    {
-        Debug.Log("Look");
-        transform.LookAt(_player.transform.position);
-        FollowPlayer();
-    }
-
-    private void FollowPlayer()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _speed *Time.deltaTime);
-        _animator.SetBool("Follow", true);
     }
 
     private void Update()
     {
-        float distance = Vector3.Distance(_player.transform.position, transform.position);
-        //if(distance > 2)
-
+        if (_player = null)
+        {
+            float distance = Vector3.Distance(_player.transform.position, transform.position);
+            if (distance < 2)
+                _agent.SetDestination(_player.transform.position);
+            else
+            {
+                Patrol();
+                _player = null;
+            }
+        }
+        if (_agent.velocity.magnitude < 0.15f)
+            Patrol();
+            
     }
 
+ 
+    private void FollowPlayer()
+    {
+        _animator.SetBool("Run", true);
+        _agent.speed = _runSpeed;
+        _agent.SetDestination(_player.transform.position);
+    }
+
+    private void Patrol()
+    {
+        StartCoroutine(ShowIdle());
+        _nextPoint = UnityEngine.Random.Range(0, _quantityPoints);
+    }
+
+    private void WalkToPoint(int nextPoint)
+    {
+        _animator.SetBool("Walk", true);
+        _agent.speed = _walkSpeed;
+        _agent.SetDestination(_patrolPoints[nextPoint].transform.position);
+    }
+
+    private IEnumerator ShowIdle()
+    {
+        _animator.SetBool("Run", false);
+        _animator.SetBool("Walk", false);
+        yield return new WaitForSeconds(2);
+        WalkToPoint(_nextPoint);
+    }
 }
