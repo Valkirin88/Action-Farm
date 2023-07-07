89e7de2a0f65b26d2fc707346896f738d521718e
Valkirin88 <Valkirin@rambler.ru>
2023-07-07 16:51:28 +0300

    Zombie animation started

--- Assets/Scripts/Enemies/Zombie.cs
+++ Assets/Scripts/Enemies/Zombie.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Zombie : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private Rigidbody _rigidBody;
+
+    [SerializeField]
+    private Animator _animator;
+
+    private GameObject _player;
+    private float _speed =1f;
+
+    private void OnTriggerStay(Collider other)
+        {
+        if (other.GetComponent<PlayerView>())
+        {
+            _player = other.gameObject;
+            CatchPlayer();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CatchPlayer()
     {
-        
+        Debug.Log("Look");
+        transform.LookAt(_player.transform.position);
+        FollowPlayer();
     }
+
+    private void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _speed *Time.deltaTime);
+        _animator.SetBool("Follow", true);
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(_player.transform.position, transform.position);
+        //if(distance > 2)
+
+    }
+
 }
